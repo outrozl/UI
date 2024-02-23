4d4113c231472218575c7d5665e1193877b9f0c7
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-22 19:13:05 -0600

    Update other.lua

--- other.lua
+++ other.lua
@@ -47,26 +47,8 @@ local RandomTextLabel = Instance.new("TextLabel")
 local Moreinfo_lol = Instance.new("TextLabel")
 local Credits = Instance.new("TextLabel")
 
-local function GetVer()
-    -- Enlace que contiene solo el número de versión
-    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
-    
-    -- Realizar una solicitud HTTP para obtener el contenido del enlace
-    local response = game.HttpGet(versionURL)
-    
-    -- Verificar si la respuesta es válida
-    if response then
-        -- Extraer el número de versión del texto obtenido
-        local NevVer = response:match("(%d+%.%d+%.%d+)")
-        return NevVer
-    else
-        -- Manejar el caso en que la respuesta no sea válida
-        return nil
-    end
-end
-
 -- Obtener la versión
-local NevVer = GetVer()
+local NevVer = "1.1.3"
 
 --Properties:
 
