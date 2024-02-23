7bdd7ac21b7ae3559a8edc653a05d5b5e281952a
Nev <153566382+outrozl@users.noreply.github.com>
2024-02-22 18:56:28 -0600

    Update other.lua

--- other.lua
+++ other.lua
@@ -32,13 +32,41 @@ local ImageLabel_2 = Instance.new("ImageLabel")
 local Risk = Instance.new("Frame")
 local UICorner_7 = Instance.new("UICorner")
 local MoreInfo_Text = Instance.new("TextLabel")
-local Translated = Instance.new("TextLabel")
 local Run = Instance.new("Frame")
 local UICorner_8 = Instance.new("UICorner")
-local TextLabel_5 = Instance.new("TextLabel")
+local Run_Text = Instance.new("TextLabel")
+local Run_Action = Instance.new("TextButton")
 local NoRun = Instance.new("Frame")
 local UICorner_9 = Instance.new("UICorner")
-local TextLabel_6 = Instance.new("TextLabel")
+local NoRun_Text = Instance.new("TextLabel")
+local NoRun_Action = Instance.new("TextButton")
+local ScriptInfo = Instance.new("Frame")
+local UICorner_10 = Instance.new("UICorner")
+local Translated = Instance.new("TextLabel")
+local RandomTextLabel = Instance.new("TextLabel")
+local Moreinfo_lol = Instance.new("TextLabel")
+local Credits = Instance.new("TextLabel")
+
+local function GetVer()
+    -- Enlace que contiene solo el número de versión
+    local versionURL = "https://raw.githubusercontent.com/outrozl/project/main/nevscripthub/scripts/tools/version.txt"
+    
+    -- Realizar una solicitud HTTP para obtener el contenido del enlace
+    local response = game.HttpGet(versionURL)
+    
+    -- Verificar si la respuesta es válida
+    if response then
+        -- Extraer el número de versión del texto obtenido
+        local NevVer = response:match("(%d+%.%d+%.%d+)")
+        return NevVer
+    else
+        -- Manejar el caso en que la respuesta no sea válida
+        return nil
+    end
+end
+
+-- Obtener la versión
+local NevVer = GetVer()
 
 --Properties:
 
@@ -109,7 +137,7 @@ ScrollingFrame.Size = UDim2.new(0, 185, 0, 185)
 UIListLayout.Parent = ScrollingFrame
 UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-UIListLayout.Padding = UDim.new(0, 2)
+UIListLayout.Padding = UDim.new(0, 4)
 
 Japonese.Name = "Japonese"
 Japonese.Parent = ScrollingFrame
@@ -289,19 +317,6 @@ MoreInfo_Text.TextScaled = true
 MoreInfo_Text.TextSize = 14.000
 MoreInfo_Text.TextWrapped = true
 
-Translated.Name = "Translated"
-Translated.Parent = Main
-Translated.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Translated.BackgroundTransparency = 1.000
-Translated.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Translated.BorderSizePixel = 0
-Translated.Size = UDim2.new(0, 125, 0, 30)
-Translated.Font = Enum.Font.SourceSans
-Translated.Text = "Translated to: English"
-Translated.TextColor3 = Color3.fromRGB(255, 255, 255)
-Translated.TextSize = 14.000
-Translated.TextWrapped = true
-
 Run.Name = "Run"
 Run.Parent = Main
 Run.BackgroundColor3 = Color3.fromRGB(255, 62, 62)
@@ -312,17 +327,32 @@ Run.Size = UDim2.new(0, 95, 0, 40)
 
 UICorner_8.Parent = Run
 
-TextLabel_5.Parent = Run
-TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_5.BackgroundTransparency = 1.000
-TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_5.BorderSizePixel = 0
-TextLabel_5.Position = UDim2.new(0.0526315793, 0, 0.125, 0)
-TextLabel_5.Size = UDim2.new(0, 84, 0, 30)
-TextLabel_5.Font = Enum.Font.SourceSans
-TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_5.TextSize = 20.000
-TextLabel_5.TextWrapped = true
+Run_Text.Name = "Run_Text"
+Run_Text.Parent = Run
+Run_Text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Run_Text.BackgroundTransparency = 1.000
+Run_Text.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Run_Text.BorderSizePixel = 0
+Run_Text.Position = UDim2.new(0.0526315793, 0, 0.125, 0)
+Run_Text.Size = UDim2.new(0, 84, 0, 30)
+Run_Text.Font = Enum.Font.SourceSans
+Run_Text.Text = "Run script"
+Run_Text.TextColor3 = Color3.fromRGB(255, 255, 255)
+Run_Text.TextScaled = true
+Run_Text.TextSize = 20.000
+Run_Text.TextWrapped = true
+
+Run_Action.Name = "Run_Action"
+Run_Action.Parent = Run
+Run_Action.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Run_Action.BackgroundTransparency = 1.000
+Run_Action.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Run_Action.BorderSizePixel = 0
+Run_Action.Size = UDim2.new(0, 95, 0, 40)
+Run_Action.Font = Enum.Font.SourceSans
+Run_Action.Text = ""
+Run_Action.TextColor3 = Color3.fromRGB(0, 0, 0)
+Run_Action.TextSize = 14.000
 
 NoRun.Name = "NoRun"
 NoRun.Parent = Main
@@ -334,28 +364,115 @@ NoRun.Size = UDim2.new(0, 95, 0, 40)
 
 UICorner_9.Parent = NoRun
 
-TextLabel_6.Parent = NoRun
-TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_6.BackgroundTransparency = 1.000
-TextLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_6.BorderSizePixel = 0
-TextLabel_6.Position = UDim2.new(0.0526315793, 0, 0.125, 0)
-TextLabel_6.Size = UDim2.new(0, 84, 0, 30)
-TextLabel_6.Font = Enum.Font.SourceSans
-TextLabel_6.TextColor3 = Color3.fromRGB(150, 150, 150)
-TextLabel_6.TextSize = 20.000
-TextLabel_6.TextWrapped = true
+NoRun_Text.Name = "NoRun_Text"
+NoRun_Text.Parent = NoRun
+NoRun_Text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NoRun_Text.BackgroundTransparency = 1.000
+NoRun_Text.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NoRun_Text.BorderSizePixel = 0
+NoRun_Text.Position = UDim2.new(0.0526315793, 0, 0.125, 0)
+NoRun_Text.Size = UDim2.new(0, 84, 0, 30)
+NoRun_Text.Font = Enum.Font.SourceSans
+NoRun_Text.Text = "No run script"
+NoRun_Text.TextColor3 = Color3.fromRGB(150, 150, 150)
+NoRun_Text.TextScaled = true
+NoRun_Text.TextSize = 20.000
+NoRun_Text.TextWrapped = true
+
+NoRun_Action.Name = "NoRun_Action"
+NoRun_Action.Parent = NoRun
+NoRun_Action.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NoRun_Action.BackgroundTransparency = 1.000
+NoRun_Action.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NoRun_Action.BorderSizePixel = 0
+NoRun_Action.Size = UDim2.new(0, 95, 0, 40)
+NoRun_Action.Font = Enum.Font.SourceSans
+NoRun_Action.Text = ""
+NoRun_Action.TextColor3 = Color3.fromRGB(0, 0, 0)
+NoRun_Action.TextSize = 14.000
+
+ScriptInfo.Name = "ScriptInfo"
+ScriptInfo.Parent = Main
+ScriptInfo.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+ScriptInfo.BackgroundTransparency = 0.100
+ScriptInfo.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ScriptInfo.BorderSizePixel = 0
+ScriptInfo.Position = UDim2.new(0, 0, 1.02641523, 0)
+ScriptInfo.Size = UDim2.new(0, 315, 0, 70)
+
+UICorner_10.Parent = ScriptInfo
+
+Translated.Name = "Translated"
+Translated.Parent = ScriptInfo
+Translated.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Translated.BackgroundTransparency = 1.000
+Translated.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Translated.BorderSizePixel = 0
+Translated.Position = UDim2.new(0.777777791, 0, 0.571428597, 0)
+Translated.Size = UDim2.new(0, 70, 0, 30)
+Translated.Font = Enum.Font.SourceSans
+Translated.Text = "English"
+Translated.TextColor3 = Color3.fromRGB(255, 255, 255)
+Translated.TextSize = 14.000
+Translated.TextWrapped = true
+
+RandomTextLabel.Name = "RandomTextLabel"
+RandomTextLabel.Parent = ScriptInfo
+RandomTextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RandomTextLabel.BackgroundTransparency = 1.000
+RandomTextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+RandomTextLabel.BorderSizePixel = 0
+RandomTextLabel.Position = UDim2.new(0.400000006, 0, 0.571428597, 0)
+RandomTextLabel.Size = UDim2.new(0, 125, 0, 30)
+RandomTextLabel.Font = Enum.Font.SourceSans
+RandomTextLabel.Text = "Translated to:"
+RandomTextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+RandomTextLabel.TextSize = 14.000
+RandomTextLabel.TextWrapped = true
+RandomTextLabel.TextXAlignment = Enum.TextXAlignment.Right
+
+Moreinfo_lol.Name = "Moreinfo_lol"
+Moreinfo_lol.Parent = ScriptInfo
+Moreinfo_lol.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Moreinfo_lol.BackgroundTransparency = 1.000
+Moreinfo_lol.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Moreinfo_lol.BorderSizePixel = 0
+Moreinfo_lol.Size = UDim2.new(0, 315, 0, 40)
+Moreinfo_lol.Font = Enum.Font.SourceSans
+Moreinfo_lol.Text = "The scripts in this script hub can have key system"
+Moreinfo_lol.TextColor3 = Color3.fromRGB(255, 255, 255)
+Moreinfo_lol.TextScaled = true
+Moreinfo_lol.TextSize = 14.000
+Moreinfo_lol.TextWrapped = true
+
+Credits.Name = "Credits"
+Credits.Parent = ScriptInfo
+Credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Credits.BackgroundTransparency = 1.000
+Credits.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Credits.BorderSizePixel = 0
+Credits.Position = UDim2.new(0, 0, 0.614285707, 0)
+Credits.Size = UDim2.new(0, 146, 0, 23)
+Credits.Font = Enum.Font.SourceSans
+Credits.Text = "Nev | Script Hub | "..NevVer
+Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits.TextSize = 18.000
+Credits.TextWrapped = true
 
 -- Scripts:
 
-local function HTQVD_fake_script() -- GUI.LocalScript 
+local function UGDSQJI_fake_script() -- GUI.LocalScript 
 	local script = Instance.new('LocalScript', GUI)
 
 	local warn_text = script.Parent.Main.Warn_Text
 	local moreinfo_text = script.Parent.Main.Risk.MoreInfo_Text
-	local langselected_text = script.Parent.Main.Translated
+	local langselected_text = script.Parent.Main.ScriptInfo.Translated
+	local runtext = script.Parent.Main.Run.Run_Text
+	local noruntext = script.Parent.Main.NoRun.NoRun_Text
+	local random = script.Parent.Main.ScriptInfo.RandomTextLabel
+	local scriptskeytext = script.Parent.Main.ScriptInfo.Moreinfo_lol
 	
-	local lang_selected = "Translated to: English"
+	local lang_selected = "English"
 	
 	local buttons = {
 		["EnglishButton"] = script.Parent.Main.Select_Lang.ScrollingFrame.English.LangAction,
@@ -367,19 +484,31 @@ local function HTQVD_fake_script() -- GUI.LocalScript
 	local translated = {
 		["English"] = {
 			warn_text = "Hello, this alert is to remind you that the scripts that may be within this script hub may not be updated or translated",
-			moreinfo_text = "We recommend using a secondary account because we do not take care of bans/Permanent Suspensions to primary accounts and remember that the scripts in this script hub cannot be updated or translated into your language"
+			moreinfo_text = "We recommend using a secondary account because we do not take care of bans/Permanent Suspensions to primary accounts and remember that the scripts in this script hub cannot be updated or translated into your language",
+			runtext = "Run script",
+			noruntext = "No run script",
+			scriptskeytext = "Scripts in this script center can have a key system"
 		},
-		["Spanish"] = {
+		["Español"] = {
 			warn_text = "Hola, esta alerta es para recordarte que los scripts que puede haber dentro de este script hub no pueden estar actualizados o traducidos",
-			moreinfo_text = "Te recomendamos usar una cuenta secundaria porque no nos hacemos cargo de baneos / Suspensiones Permanentes a cuentas principales y recuerda que los scripts en este script hub no pueden estar actualizados o traducidos a tu idioma"
+			moreinfo_text = "Te recomendamos usar una cuenta secundaria porque no nos hacemos cargo de baneos / Suspensiones Permanentes a cuentas principales y recuerda que los scripts en este script hub no pueden estar actualizados o traducidos a tu idioma",
+			runtext = "Cargar script",
+			noruntext = "No cargar el script",
+			scriptskeytext = "Los scripts de este centro de scripts pueden tener un sistema de claves"
 		},
 		["Portuguese"] = {
 			warn_text = "Olá, este alerta é para lembrá-lo de que os scripts que podem estar dentro deste hub de script podem não ser atualizados ou traduzidos",
-			moreinfo_text = "Recomendamos o uso de uma conta secundária porque não cuidamos de banimentos/suspensões permanentes de contas primárias e lembramos que os scripts neste hub de script não podem ser atualizados ou traduzidos para o seu idioma"
+			moreinfo_text = "Recomendamos o uso de uma conta secundária porque não cuidamos de banimentos/suspensões permanentes de contas primárias e lembramos que os scripts neste hub de script não podem ser atualizados ou traduzidos para o seu idioma",
+			runtext = "Carregar script",
+			noruntext = "Não carregue o script",
+			scriptskeytext = "Os scripts neste centro de scripts podem ter um sistema de chaves"
 		},
 		["Japonese"] = {
 			warn_text = "こんにちは、このアラートは、このスクリプト ハブ内にある可能性のあるスクリプトが更新または変換されない可能性があることを通知するためのものです",
-			moreinfo_text = "プライマリ アカウントへの禁止/永久停止は行わず、このスクリプト ハブのスクリプトを更新したり、お客様の言語に翻訳したりすることはできません"
+			moreinfo_text = "プライマリ アカウントへの禁止/永久停止は行わず、このスクリプト ハブのスクリプトを更新したり、お客様の言語に翻訳したりすることはできません",
+			runtext = "スクリプトをロードする",
+			noruntext = "スクリプトをロードしないでください",
+			scriptskeytext = "このスクリプト センターのスクリプトにはキー システムを含めることができます"
 		},
 		-- Agrega traducciones para otros idiomas aquí
 	}
@@ -388,8 +517,14 @@ local function HTQVD_fake_script() -- GUI.LocalScript
 	local function updateText()
 		local translation = translated[lang_selected]
 		if translation then
+			random.Text = "Translating to:"
+			wait(2)
 			warn_text.Text = translation.warn_text
 			moreinfo_text.Text = translation.moreinfo_text
+			runtext.Text = translation.runtext
+			noruntext.Text = translation.noruntext
+			random.Text = "Translated to:"
+			scriptskeytext.Text = translation.scriptskeytext
 		else
 			warn_text.Text = "Translation not available for selected language."
 		end
@@ -398,37 +533,41 @@ local function HTQVD_fake_script() -- GUI.LocalScript
 	local function updatelangselected()
 		while true do
 			langselected_text.Text = lang_selected
-			wait(1)  -- Puedes ajustar el intervalo de actualización según tus necesidades
+			wait(.3)
 		end
 	end
 	
 	-- Función para cambiar el idioma seleccionado
 	local function changeLanguage(language)
-		lang_selected = language
-		updateText()
+		if lang_selected == language then
+			print("Nev Script Hub: Cant Translate")
+		else
+			lang_selected = language
+			updateText()
+		end
 	end
 	
 	buttons.ESButton.MouseButton1Up:Connect(function()
-		changeLanguage("Spanish")
-		lang_selected = "Translated to: Español"
+		changeLanguage("Español")
+		lang_selected = "Español"
 	end)
 	
 	buttons.EnglishButton.MouseButton1Up:Connect(function()
 		changeLanguage("English")
-		lang_selected = "Translated to: English"
+		lang_selected = "English"
 	end)
 	
 	buttons.PRButton.MouseButton1Up:Connect(function()
 		changeLanguage("Portuguese")
-		lang_selected = "Translated to: Portuguese"
+		lang_selected = "Portuguese"
 	end)
 	
 	buttons.JAButton.MouseButton1Up:Connect(function()
 		changeLanguage("Japonese")
-		lang_selected = "Translated to: Japonese"
+		lang_selected = "Japonese"
 	end)
 	
 	-- Llama a la función en un hilo separado para que no bloquee la ejecución principal
 	spawn(updatelangselected)
 end
-coroutine.wrap(HTQVD_fake_script)()
+coroutine.wrap(UGDSQJI_fake_script)()
